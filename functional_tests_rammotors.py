c23b93632b4f62e2f053969dd9ffbdde7b4b36a5
Samir Sadek <netsamir@gmail.com>
2016-11-26 08:11:31 +0100

    Adding specification for functional test and use unittest

--- functional_tests_rammotors.py
+++ functional_tests_rammotors.py
@@ -2,9 +2,36 @@
 Functional testing for Ram Motors
 """
 
+import unittest
 from selenium import webdriver
 
-BROWSER = webdriver.Firefox()
-BROWSER.get('http://localhost:8000')
+class NewAdminTest(unittest.TestCase):
+    """Testing the ability to add a vehicule and to
+    check it in the list of vehicule"""
 
-assert 'Django' in BROWSER.title
+    def setUp(self):
+        """ Docstring """
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        """ Docstring """
+        self.browser.quit()
+
+    def test_can_add_vehicule_check(self):
+        """ Docstring """
+        self.browser.get('http://localhost:8000')
+
+        # The Customer check that the title refer to his company
+        assert 'Ram Motors' in self.browser.title,\
+        'Browse title was: ' + self.browser.title
+
+        self.fail('Finish the test!')
+        # He is invited to add a new car
+        # Brand, Model, year, km, pic features, other picture
+
+        # He save and then he is presented with the list of already
+        # saved car
+
+if __name__ == '__main__':
+    unittest.main()
