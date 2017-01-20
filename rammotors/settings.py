c2784379f6988ff6dadcc4f3fed02b4b39f261cd
Samir Sadek <netsamir@gmail.com>
2017-01-20 21:12:32 +0100

    Remove unused packate from settings.py

--- rammotors/settings.py
+++ rammotors/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'rammotors.urls'
