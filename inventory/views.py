a620fe2834e7756b66b880406293cd215498cf5a
Samir Sadek <netsamir@gmail.com>
2017-02-02 07:55:50 +0100

    Adding filtering for the brands

--- inventory/views.py
+++ inventory/views.py
@@ -1,13 +1,29 @@
 """ Views for Inventory"""
+
+from collections import defaultdict
+
 from django.shortcuts import render
 
 from inventory import services
 
 # Create your views here.
 
+def filter_brands(vehicles):
+    """ Group the cars by brands """
+    brands = defaultdict(int)
+    for vhc in vehicles:
+        brands[vhc.brand] += 1
+    return dict(brands)
+
 def vehicules_list(request):
+    """ Return the context and render templates """
     autoscout = services.WsdlAutoscout24()
     images_uri = autoscout.uri_images('main')
     vehicles = autoscout()
-    context = {'vehicles': vehicles, 'images_uri': images_uri}
+    brands = filter_brands(vehicles)
+    context = {\
+        'vehicles': vehicles,
+        'images_uri': images_uri,
+        'brands': brands\
+    }
     return render(request, 'inventory/list_cars.html', context)
