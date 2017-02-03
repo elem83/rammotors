7528f6bad0ee5ce2e74d2b09c2e7d01c76e0e8f7
Samir Sadek <netsamir@gmail.com>
2017-02-03 15:34:21 +0100

    remove get_enumeration during deploy; add a template default if none

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ git pull origin master
 ./manage.py collectstatic --noinput  --settings=rammotors.settings.staging
 ./manage.py makemigrations --settings=rammotors.settings.staging
 ./manage.py migrate --settings=rammotors.settings.staging
-python manage.py get_enumerations --settings=rammotors.settings.staging
+# python manage.py get_enumerations --settings=rammotors.settings.staging
 ../apache2/bin/restart
