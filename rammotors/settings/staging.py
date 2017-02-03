6f4b45abe80ccee6cc286685fe39b2777082801c
Samir Sadek <netsamir@gmail.com>
2017-02-03 11:54:49 +0100

    typo in database name

--- rammotors/settings/staging.py
+++ rammotors/settings/staging.py
@@ -15,7 +15,7 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.child('sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
