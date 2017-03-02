94848332035c4d0f6bc30ee251577ba7e5cd74c6
Samir Sadek <netsamir@gmail.com>
2017-03-02 18:15:23 +0100

    Test view one pass

--- inventory/tests/test_views.py
+++ inventory/tests/test_views.py
@@ -1,20 +1,30 @@
-# pylint: disable=no-member, protected-access
+# pylint: disable=unused-argument, no-member, protected-access
 """Unit test for Inventory"""
+
+from unittest.mock import patch
+
+from mixer.backend.django import mixer
+import pytest # pylint: disable=unused-import
 
 from django.urls import resolve
 from django.test import RequestFactory
 #from django.http import HttpRequest
 #from django.template.loader import render_to_string
 
-import pytest # pylint: disable=unused-import
-
 from inventory.views import vehicles_list
+from inventory import services
+from inventory.tests.test_services import (get_lookup_mock, find_articles_mock,
+                                           get_article_mock)
+
 
 pytestmark = pytest.mark.django_db # pylint: disable=invalid-name
 
-@pytest.mark.xfail(run=False)
-def test_vehicles_url_resolve():
+@patch('inventory.services.lookup', side_effect=get_lookup_mock)
+@patch('inventory.services.find_articles', side_effect=find_articles_mock)
+def test_vehicles_url_resolve(mock_find_articles, mock_lookup):
     """Resolve the vehicles URL"""
+    for elem in services.AS24WSSearch().get_lookup_data():
+        mixer.blend('inventory.Enumeration', **elem)
     found = resolve('/')
     assert found.func == vehicles_list, \
             "Should find the list of vehicles"
@@ -23,6 +33,7 @@ def test_vehicles_url_resolve():
     assert response.status_code == 200, \
             "Should be callable by anyone"
 """
+@patch('inventory.services.get_article_details', side_effect=get_article_mock)
 def test_vehicles_return_html()
     request = HttpRequest()
     response = vehicles_list(request)
