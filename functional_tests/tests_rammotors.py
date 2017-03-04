68a07c193c08f12d254a6dc282c7cd26e257083c
Samir Sadek <netsamir@gmail.com>
2017-03-04 06:48:38 +0100

    Selenium changing framework from unittest to pytest -- manage to setup/teardown

--- functional_tests/tests_rammotors.py
+++ functional_tests/tests_rammotors.py
@@ -1,40 +1,26 @@
+# pylint: disable=unused-argument, redefined-outer-name, missing-docstring
 """
 Functional testing for Ram Motors
 """
 
-import unittest
+import pytest
+
 from selenium import webdriver
 #from selenium.webdriver.common.keys import Keys
 
-class NewAdminTest(unittest.TestCase):
-    """Testing the ability to add a vehicule and to
-    check it in the list of vehicule"""
+@pytest.fixture(scope="module")
+def browser(request):
+    selenium = webdriver.Firefox()
+    selenium.implicitly_wait(3)
+    def teardown():
+        selenium.quit()
+    request.addfinalizer(teardown)
+    return selenium
 
-    def setUp(self):
-        """ Docstring """
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+def test_browsing_check(browser):
+    """ Docstring """
+    browser.get('http://localhost:8000')
 
-    def tearDown(self):
-        """ Docstring """
-        self.browser.quit()
-
-    def test_can_add_vehicule_check(self):
-        """ Docstring """
-        self.browser.get('http://localhost:8000/')
-
-        # The Customer check that the title refer to his company
-        assert 'Ram Motors' in self.browser.title,\
-        'Browse title was: ' + self.browser.title
-
-        # The user is happy because the developer did not need
-        # any input from him.  All the cars are retrieved from
-        # the websites through wsdl.
-        # He goes to the site and check that all the cars are there
-
-        self.fail('Finish the test!')
-        # He save and then he is presented with the list of already
-        # saved car
-
-if __name__ == '__main__':
-    unittest.main()
+    # Checking that title is right
+    assert 'Ram Motors' in browser.title,\
+    'Browse title was: ' + browser.title
