a971c95cf1fbd8173ff1d0e5bdbc7dfe22a117d7
Samir Sadek <netsamir@gmail.com>
2017-03-05 04:58:37 +0100

    first test of filter

--- functional_tests/tests_rammotors.py
+++ functional_tests/tests_rammotors.py
@@ -20,39 +20,45 @@ def browser(request):
     request.addfinalizer(teardown)
     return selenium
 
-def home(browser):
+reported_cars = lambda browser: \
+        int(browser.find_element_by_id('count_car').text)
+
+def visible_cars(browser, css_selector): \
+    return len([x for x in \
+        browser.find_elements_by_class_name('list-product-description') \
+        if x.is_displayed()])
+
+def reset(browser):
     browser.get('http://localhost:8000')
 
 def assert_title(browser):
     assert 'Ram Motors' in browser.title,\
     'Browse title was: ' + browser.title
 
+def assert_reported_vs_listed(browser, css_selector):
+    assert visible_cars(browser, css_selector) == reported_cars(browser), \
+    "The number of cars reported should be equal to the number of cars display"
+
 def move_to(browser, css_selector):
+    WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector))
+    )
+
     browser.find_element_by_css_selector(css_selector).click()
-
-reported_cars = lambda browser: \
-        int(browser.find_element_by_id('count_car').text)
-
-visible_cars = lambda browser, css_selector: \
-        len(browser.find_elements_by_class_name('list-product-description'))
 
 def test_browsing_check(browser):
     """ Docstring """
-    home(browser)
+    reset(browser)
     assert_title(browser)
-
-    assert visible_cars(browser, 'list-product-description') == reported_cars(browser), \
-    "The number of cars reported should be equal to the number of cars display"
-
+    assert_reported_vs_listed(browser, 'list-product-description')
     move_to(browser, 'ul.list-inline a[href="/grid/"]')
-
-    assert visible_cars(browser, 'div.filter-results div.col-md-4') == reported_cars(browser), \
-    "The number of cars reported should be equal to the number of cars display"
-
-    WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, 'ul.list-inline a[href="/"]'))
-    )
-
+    assert_reported_vs_listed(browser, 'div.filter-results div.col-md-4')
     move_to(browser, 'ul.list-inline a[href="/"]')
-    assert visible_cars(browser, 'list-product-description') == reported_cars(browser), \
-    "The number of cars reported should be equal to the number of cars display"
+    assert_reported_vs_listed(browser, 'list-product-description')
+
+def test_filters(browser):
+    reset(browser)
+    move_to(browser, 'li > label.checkbox')
+    import time
+    time.sleep(4)
+    assert_reported_vs_listed(browser, 'list-product-description')
