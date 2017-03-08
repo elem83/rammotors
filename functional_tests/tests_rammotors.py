92817b8064b7241bd1ffa55f20c85df72cb83d00
Samir Sadek <netsamir@gmail.com>
2017-03-08 06:20:59 +0100

    refactor the test filters/sort

--- functional_tests/tests_rammotors.py
+++ functional_tests/tests_rammotors.py
@@ -42,11 +42,6 @@ def assert_reported_vs_visible(browser, css_selector):
     assert visible_cars(browser, css_selector) == reported_cars(browser), \
     "The number of cars reported should be equal to the number of cars display"
 
-# def move_to(browser, css_selector):
-#    WebDriverWait(browser, 10).until(
-#        EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector))
-#    )
-
 def test_links(browser):
     browser.get('http://localhost:8000/grid/')
 
@@ -55,103 +50,73 @@ def test_links(browser):
     browser.find_element_by_css_selector('ul.list-inline a[href="/"]').click()
     assert_title(browser)
 
-def test_list_pages(browser):
-    browser.get('http://localhost:8000')
+def helper_filters(browser, css_selector_count):
 
-    # Test entry page
-    assert_reported_vs_visible(browser, 'list-product-description')
-
-    # Test filters
     filters = browser.find_elements_by_css_selector('li > label.checkbox')
     assert len(filters) != 0, 'The number of filters should not be null'
     for f in filters:
         f.click()
-        wait_for_count(browser, 'list-product-description')
-        assert_reported_vs_visible(browser, 'list-product-description')
+        wait_for_count(browser, css_selector_count)
+        assert_reported_vs_visible(browser, css_selector_count)
 
     browser.find_element_by_id('reset').click()
-    assert_reported_vs_visible(browser, 'list-product-description')
+    assert_reported_vs_visible(browser, css_selector_count)
+
+def helper_sort(browser, css_id_key, css_selector_count, page='list'):
+
+    browser.find_element_by_id('sort_criteria').click()
+    browser.find_element_by_css_selector('li[data-sort-by="{}"]'.format(css_id_key)).click()
+
+    WebDriverWait(browser, 10).until(\
+    lambda browser:\
+        EC.visibility_of_element_located((By.CLASS_NAME, css_id_key)) \
+        and visible_cars(browser, css_selector_count) == reported_cars(browser))
+
+    if page == 'list':
+        elems_raw = [(x.location['y'], int(x.text.replace(",", ""))) for x in \
+                browser.find_elements_by_class_name(css_id_key)]
+
+        elems = [x[1] for x in sorted(elems_raw, key=lambda pos: pos[0])]
+        assert all(elems[i] <= elems[i+1] for i in range(len(elems)-1)), \
+            "The list should be sorted"
+    else: # grid
+        elems_raw = [(x.location['x'], x.location['y'], int(x.text.replace(",", ""))) for x in \
+                browser.find_elements_by_class_name('km')]
+
+        elems = [x[2] for x in sorted(elems_raw, key=lambda pos: (pos[1], pos[0]))]
+        assert all(a <= b  for a, b in zip(elems, elems[:1])), \
+        'The list should be sorted'
+
+def test_list_pages(browser):
+    browser.get('http://localhost:8000')
+
+    css_selector_count = 'list-product-description'
+    # Test entry page
+    assert_reported_vs_visible(browser, css_selector_count)
+
+    # Test filters
+    helper_filters(browser, css_selector_count)
 
     # Test sort km
-    browser.find_element_by_id('sort_criteria').click()
-    browser.find_element_by_css_selector('li[data-sort-by="km"]').click()
-
-    WebDriverWait(browser, 10).until(\
-    lambda browser:\
-        EC.visibility_of_element_located((By.CLASS_NAME, 'km')) \
-        and visible_cars(browser, 'list-product-description') == reported_cars(browser))
-
-    elems_raw = [(x.location['y'], int(x.text.replace(",", ""))) for x in \
-             browser.find_elements_by_class_name('km')]
-
-    elems = [x[1] for x in sorted(elems_raw, key=lambda pos: pos[0])]
-    assert all(elems[i] <= elems[i+1] for i in range(len(elems)-1)), \
-        "The list should be sorted"
+    helper_sort(browser, 'km', css_selector_count)
 
     # Test sort price
-    browser.find_element_by_id('sort_criteria').click()
-    browser.find_element_by_css_selector('li[data-sort-by="price"]').click()
-
-    WebDriverWait(browser, 10).until(\
-    lambda browser:\
-        EC.visibility_of_element_located((By.CLASS_NAME, 'price')) \
-        and visible_cars(browser, 'list-product-description') == reported_cars(browser))
-
-    elems_raw = [(x.location['y'], int(x.text.replace(",", ""))) for x in \
-             browser.find_elements_by_class_name('price')]
-
-    elems = [x[1] for x in sorted(elems_raw, key=lambda pos: pos[0])]
-    assert all(elems[i] <= elems[i+1] for i in range(len(elems)-1)), \
-        "The list should be sorted"
+    helper_sort(browser, 'price', css_selector_count)
 
 def test_grid_pages(browser):
     browser.get('http://localhost:8000/grid/')
     assert_title(browser)
 
+    css_selector_count = 'product-description'
+
     # Test entry page
-    assert_reported_vs_visible(browser, 'product-description')
+    assert_reported_vs_visible(browser, css_selector_count)
 
     # Test filters
-    filters = browser.find_elements_by_css_selector('li > label.checkbox')
-    assert len(filters) != 0, 'The number of filters should not be null'
-    for f in filters:
-        f.click()
-        wait_for_count(browser, 'product-description')
-        assert_reported_vs_visible(browser, 'product-description')
-
-    browser.find_element_by_id('reset').click()
-    wait_for_count(browser, 'product-description')
-    assert_reported_vs_visible(browser, 'product-description')
+    helper_filters(browser, css_selector_count)
 
     # Test sort km
-    browser.find_element_by_id('sort_criteria').click()
-    browser.find_element_by_css_selector('li[data-sort-by="km"]').click()
-
-    WebDriverWait(browser, 10).until(\
-    lambda browser:\
-        EC.visibility_of_element_located((By.CLASS_NAME, 'km')) \
-        and visible_cars(browser, 'product-description') == reported_cars(browser))
-
-    elems_raw = [(x.location['x'], x.location['y'], int(x.text.replace(",", ""))) for x in \
-             browser.find_elements_by_class_name('km')]
-
-    elems = [x[2] for x in sorted(elems_raw, key=lambda pos: (pos[1], pos[0]))]
-    assert all(a <= b  for a, b in zip(elems, elems[:1])), \
-     'The list should be sorted'
+    helper_sort(browser, 'km', css_selector_count, page='grid')
 
     # Test sort price
-    browser.find_element_by_id('sort_criteria').click()
-    browser.find_element_by_css_selector('li[data-sort-by="price"]').click()
-
-    WebDriverWait(browser, 10).until(\
-    lambda browser:\
-        EC.visibility_of_element_located((By.CLASS_NAME, 'price')) \
-        and visible_cars(browser, 'product-description') == reported_cars(browser))
-
-    elems_raw = [(x.location['x'], x.location['y'], int(x.text.replace(",", ""))) for x in \
-             browser.find_elements_by_class_name('price')]
-
-    elems = [x[2] for x in sorted(elems_raw, key=lambda pos: (pos[1], pos[0]))]
-    print(elems)
-    assert all(a <= b  for a, b in zip(elems, elems[:1])), \
-        "The list should be sorted"
+    helper_sort(browser, 'price', css_selector_count, page='grid')
